5d570f9cb3ac1200bc31f132d04477041540a036
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-09-19 09:39:17 +0530

    Added player controls event listners

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,63 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float initialPlayerSpeed = 4f;
+    [SerializeField] private float maximumPlayerSpeed = 30f;
+    [SerializeField] private float playerSpeedIncreaseRate = 0.1f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float initialGravityValue = -9.81f;
+
+    private float gravity;
+    private float playerSpeed;
+    private Vector3 movementDirection = Vector3.forward;
+
+    private PlayerInput playerInput;
+    private InputAction slideAction;
+    private InputAction turnAction;
+    private InputAction jumpAction;
+
+    private CharacterController controller;
+
+    public void Awake()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        controller = GetComponent<CharacterController>();
+
+        turnAction = playerInput.actions["Turn"];
+        jumpAction = playerInput.actions["Jump"];
+        slideAction = playerInput.actions["Slide"];
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        gravity = initialGravityValue;
+        playerSpeed = initialPlayerSpeed;
+
     }
+
+    private void OnEnable()
+    {
+        turnAction.performed += PlayerTurn;
+        jumpAction.performed += PlayerJump;
+        slideAction.performed += PlayerSlide;
+    }
+
+    private void OnDisable()
+    {
+        turnAction.performed -= PlayerTurn;
+        jumpAction.performed -= PlayerJump;
+        slideAction.performed -= PlayerSlide;
+    }
+
+    private void PlayerTurn(InputAction.CallbackContext context) { }
+    private void PlayerJump(InputAction.CallbackContext context) { }
+
+    private void PlayerSlide(InputAction.CallbackContext context) { }
+
 }
