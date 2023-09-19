865d1e44e015d897efe8cdcdf2e54fc65738a83b
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-09-19 08:47:45 +0530

    Fixed tile spawner

--- Assets/Scripts/TileSpawner.cs
+++ Assets/Scripts/TileSpawner.cs
@@ -34,16 +34,59 @@ namespace SwingTheRoad
                 SpawnTile(startingTile.GetComponent<Tile>());
             }
 
-            SpawnTile(SelectRandomGameObjectFromList(turnTiles).GetComponent<Tile>());
+            // SpawnTile(SelectRandomGameObjectFromList(turnTiles).GetComponent<Tile>());
+            SpawnTile(turnTiles[0].GetComponent<Tile>());
+            AddNewDirection(Vector3.left);
         }
 
         private void SpawnTile(Tile tile, bool spawnObstacle = false)
         {
-            Quaternion newTileRotation = tile.gameObject.transform.rotation * Quaternion.LookRotation(currentTileLocation, Vector3.up);
+            Quaternion newTileRotation = tile.gameObject.transform.rotation * Quaternion.LookRotation(currentTileDirection, Vector3.up);
 
             prevTile = GameObject.Instantiate(tile.gameObject, currentTileLocation, newTileRotation);
             currentTiles.Add(prevTile);
-            currentTileLocation += Vector3.Scale(prevTile.GetComponent<Renderer>().bounds.size, currentTileDirection);
+
+            if (tile.type == TileType.STRAIGHT)
+            {
+                currentTileLocation += Vector3.Scale(prevTile.GetComponent<Renderer>().bounds.size, currentTileDirection);
+
+            }
+        }
+
+        public void DeletePreviousTiles()
+        {
+            // use object pooling
+
+        }
+
+        public void AddNewDirection(Vector3 direction)
+        {
+            currentTileDirection = direction;
+            DeletePreviousTiles();
+
+            Vector3 tilePlacementScale;
+            if (prevTile.GetComponent<Tile>().type == TileType.SIDEWAYS)
+            {
+                tilePlacementScale = Vector3.Scale(prevTile.GetComponent<Renderer>().bounds.size / 2 +
+                    (Vector3.one * startingTile.GetComponent<BoxCollider>().size.z / 2), currentTileDirection);
+
+            }
+            else
+            {
+                tilePlacementScale = Vector3.Scale((prevTile.GetComponent<Renderer>().bounds.size -
+                 (Vector3.one * 2)) + (Vector3.one * startingTile.GetComponent<BoxCollider>().size.z / 2), currentTileDirection);
+
+            }
+
+            currentTileLocation += tilePlacementScale;
+            int currentPathLenght = Random.Range(minStraightTiles, maxStraightTiles);
+
+            for (int i = 0; i < currentPathLenght; i++)
+            {
+                SpawnTile(startingTile.GetComponent<Tile>(), i != 0);
+            }
+
+            SpawnTile(SelectRandomGameObjectFromList(turnTiles).GetComponent<Tile>(), false);
         }
 
         private GameObject SelectRandomGameObjectFromList(List<GameObject> list)
