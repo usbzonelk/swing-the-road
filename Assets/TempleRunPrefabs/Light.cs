2faf58875223da0a61e7ff5fb5b65154a3cc6381
Bhashith Udawaththa <bhashith.me@gmail.com>
2023-11-30 11:15:49 +0530

    Added Color dome prefabs

---
+++ Assets/TempleRunPrefabs/Light.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class LightColorChanger : MonoBehaviour
+{
+
+
+    public static void domeColorChanger(Light playerColorChangerLight, Color domeColor)
+    {
+        playerColorChangerLight.color = domeColor;
+
+    }
+     
+    public static void domeSpawner()
+    {
+
+    }
+
+}
